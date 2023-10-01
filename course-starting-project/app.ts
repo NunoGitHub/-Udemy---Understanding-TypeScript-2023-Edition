36456063c68e99690ae38786c1ba03162ec027cb
NunoGitHub <nunoaapp22@hotmail.com>
2023-10-01 11:17:00 +0100

    some basics, types and enum

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,34 @@
-console.log('Your code goes here...');
+/*const person : {
+    name:string;
+    age:number;
+    hobbies: string[];
+    role: [number, string];//tupple
+}= {
+  name: "Meximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role:[2, 'author']
+};*/
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+};
+
+const person ={
+  name: "Meximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN
+};
+
+let favoriteActivities: any[];
+console.log(person.age);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLowerCase());
+}
+
+if(person.role === Role.AUTHOR){
+    console.log('is author');
+}
